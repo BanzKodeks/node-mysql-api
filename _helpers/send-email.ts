aa142837ccb974baecac6fc31c497972f3e91c05
Vans Gonzales <vanzmckly@gmail.com>
2026-05-21 09:58:27 +0800

    fixed send email

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -1,13 +1,27 @@
 import nodemailer from 'nodemailer';
 
-export default async function sendEmail({ to, subject, html, from = process.env.EMAIL_FROM}: any) {
+export default async function sendEmail({ to, subject, html, from = process.env.EMAIL_FROM }: any) {
+
+    console.log('Creating transporter...');
+
     const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
-    }
+        host: process.env.SMTP_HOST,
+        port: Number(process.env.SMTP_PORT),
+        secure: false,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
     });
-    await transporter.sendMail({ from, to, subject, html});
+
+    console.log('Sending email to:', to);
+
+    const info = await transporter.sendMail({
+        from,
+        to,
+        subject,
+        html
+    });
+
+    console.log('Email sent:', info.messageId);
 }
