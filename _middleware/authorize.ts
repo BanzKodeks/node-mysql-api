72a95cdc1bd6c12ef54c7e01d8dc1b9beb26fad8
Vans Gonzales <vanzmckly@gmail.com>
2026-05-21 12:56:56 +0800

    Fix authorize middleware for express-jwt

--- _middleware/authorize.ts
+++ _middleware/authorize.ts
@@ -1,4 +1,4 @@
-import { expressjwt as jwt }  from 'express-jwt';
+import { expressjwt as jwt } from 'express-jwt';
 import db from '../_helpers/db';
 
 const secret = process.env.JWT_SECRET as string;
@@ -10,16 +10,23 @@ export default function authorize(roles: any = []) {
 
     return [
         jwt({ secret, algorithms: ['HS256'] }),
+
         async (req: any, res: any, next: any) => {
-            const account = await db.Account.findByPk(req.user.id);
+            const account = await db.Account.findByPk(req.auth.sub);
 
             if (!account || (roles.length && !roles.includes(account.role))) {
                 return res.status(401).json({ message: 'Unauthorized' });
             }
 
-            req.user.role = account.role;
-            const refreshTokens = await account.getRefreshTokens();
-            req.user.ownsToken = (token: any) => !!refreshTokens.find((x: any) => x.token === token);
+            req.user = {
+                id: account.id,
+                role: account.role,
+                ownsToken: async (token: any) => {
+                    const refreshTokens = await account.getRefreshTokens();
+                    return !!refreshTokens.find((x: any) => x.token === token);
+                }
+            };
+
             next();
         }
     ];
